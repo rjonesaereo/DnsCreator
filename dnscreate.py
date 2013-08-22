e1bf6c154c26f62a23701772770717dac631d87d
Ryan Jones <rjones@aereo.com>
2013-08-22 15:27:46 -0400

    scrapped gdata for gspread

--- dnscreate.py
+++ dnscreate.py
@@ -2,8 +2,9 @@
 
 from optparse import OptionParser
 import wmi
-import gdata
+import gspread
 import getpass
+import 
 
 #options
 
@@ -13,38 +14,37 @@ usage = OptionParser(usage = usage)
 parser = OptionParser()
 parser.add_option("-u", "--url", action="store", type="string", dest="sheetURL") # 
 parser.add_option("-e", "--email", action="store", dest="username")
-# parser.add_option("-p", )
+parser.add_option("-k","--key", action="store", dest="key" )
 
 (options, args) = parser.parse_args()
 
-# 
+#connect to google
 
-# connect to Google
-def gdata :
-		gd_client = gdata.spreadsheet.servvice.SpreadsheetService()
-		gd_client.email = username
-		gd_client.password = getpass.getpass("Enter your password") 
-		gd_client.source = sheetURL # pass this in as an option
-		try:
-			gd_client.ProgrammaticLogin() #log in
-		except socket.sslerror, e:
-			logging.error('Spreadsheet socket.sslerror: ' + str(e))
-			return False
-		key = "key"	
-        wksht_id "wksht_id"
-        q = gdata.spredsheet.service.ListQuery()
-        q.orderby = 'colum:Hostname '
-        q.reverse = 'true'
-        try:
-			    # fetch the spreadsheet data
-		            feed = gd_client.GetListFeed(key, wksht_id, query=q)
-		except gdata.service.RequestError, e:
-		            logging.error('Spreadsheet gdata.service.RequestError: ' + str(e))
-		            return False
-	    except socket.sslerror, e:
-		            logging.error('Spreadsheet socket.sslerror: ' + str(e))
-		return False
-		for row_entry in feed.entry:
-			    # to get the column data out, you use the text_db.Record class, then use the dict record.content
-		            record = gdata.spreadsheet.text_db.Record(row_entry=row_entry)
+email =getpass.getuser() +"@aereo.com"
+password = getpass.getpass("Enter your Password")
+
+def connect:(emai, password): # connect and open shee and workbook
+	sheet = gspread.login( email, password)
+	print " enter name of the spreadheet"
+	 filename = raw_input()
+	sheet.open(filename)
+	print " enter worksheet"
+	worksheet = raw_input()
+    table = sheet.worksheet
+# get the data that you want to get from each sell
+def get_data:
+	hostname = table.range('G2:G34')
+	ipmi = table.range("J2:J34")
+	private_ip = table.range('K2:K34')
+	
+	
+
+
+
+		
+
+		
+
+
+
 		
